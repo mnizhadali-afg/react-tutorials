7ce7b7032bcf13e40abc314ff5a0689b7c19201d
mnizhadali-afg <mnizhadali@gmail.com>
2023-09-03 23:29:39 +0200

    added rotating border

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,13 @@ const About = () => {
     <section className="about flex items-center" id="about">
       <div className="container">
         <div className="userDetails flex flex-direction-column items-center justify-center">
-          <img
-            src="https://avatars.githubusercontent.com/u/66347282?v=4"
-            alt="https://avatars.githubusercontent.com/u/66347282?v=4"
-          />
+          <div className="image-container">
+            <div className="rotating-border"></div>
+            <img
+              src="https://avatars.githubusercontent.com/u/66347282?v=4"
+              alt="https://avatars.githubusercontent.com/u/66347282?v=4"
+            />
+          </div>
           <h2>Hi 👋</h2>
           <h3>I'm Razia</h3>
           <p>A Frontend Developer in Kabul</p>
